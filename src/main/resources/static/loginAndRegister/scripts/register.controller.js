b79b86bad11a3adc809921fdcfa0fe8df692590c
Emil Båth <emilbath@gmail.com>
2016-10-19 09:44:21 +0200

    registrerar användare - frontendsidan

--- src/main/resources/static/loginAndRegister/scripts/register.controller.js
+++ src/main/resources/static/loginAndRegister/scripts/register.controller.js
@@ -1,16 +1,16 @@
-
 if (!loginAndRegister)
     var loginAndRegister = {};
 
 if (!loginAndRegister.register)
     loginAndRegister.register = {};
 
-loginAndRegister.register.RegisterController = function (registerService) {
-    this.addUser = function() {
+loginAndRegister.register.RegisterController = function (registerService, $location) {
+    this.addUser = function () {
         this.loading = true;
-        registerService.addUser(this.username,this.firstname,this.lastname,this.email,this.password1);
+        registerService.addUser(this.username, this.firstname, this.lastname, this.email, this.password1);
         this.loading = false;
-    }
+        $location.path("/login");
+    };
 
     this.username;
     this.firstname;
@@ -19,4 +19,4 @@ loginAndRegister.register.RegisterController = function (registerService) {
     this.password1;
     this.password2;
     this.loading = false;
-}
+};
