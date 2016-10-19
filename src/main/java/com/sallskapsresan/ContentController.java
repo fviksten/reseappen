f6e3303d2a5a9edb99cdf945163e2a83db8ffbf6
Martin Portin <martinportin@gmail.com>
2016-10-19 13:29:58 +0200

    Commit with working adduser post function and simple validation.

--- src/main/java/com/sallskapsresan/ContentController.java
+++ src/main/java/com/sallskapsresan/ContentController.java
@@ -2,9 +2,12 @@ package com.sallskapsresan;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by Administrator on 2016-10-18.
@@ -16,10 +19,12 @@ public class ContentController {
 
     //recieves, validates and directs new registered users' details to sqlrepository
     @PostMapping("/adduser")
-    public ResponseEntity<HttpStatus> addUser(@RequestBody User user) {
-        System.out.println(user.getFirstname());
-        System.out.println(user.getLastname());
-        System.out.println(user.getUsername());
-        return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+    public ResponseEntity<String> addUser(@RequestBody @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<String>("Invalid information!", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<String>("Everything went fine!", HttpStatus.OK);
+        }
     }
 }
