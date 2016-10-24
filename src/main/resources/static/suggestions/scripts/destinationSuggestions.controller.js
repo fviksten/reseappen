ad46103f33f64619c59e1c350f7a82771372eac7
Josefin Karlgrund <josefin.karlgrund@gmail.com>
2016-10-24 17:03:36 +0200

    Created "suggestions" to show the user suggested destinations (JavaScript)

--- src/main/resources/static/suggestions/scripts/destinationSuggestions.controller.js
+++ src/main/resources/static/suggestions/scripts/destinationSuggestions.controller.js
@@ -8,12 +8,10 @@ suggestions.destinationSuggestions.destinationSuggestionsController = function(d
     var self = this;
 
     this.getObject = function () {
-        console.log('ready to sent HTTPGetObject');
+
         console.log($rootScope.user);
         $http.post("/mySuggestions",$rootScope.user).then(function (response) {
                 self.object = response.data;
-                console.log(self.object);
-                console.log('inside http get...');
                 // går att kontrollera http-responsen ifall usern är fel.
             });
     }
