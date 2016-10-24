9ab2cfce566ed5f9a0d3cf1889ceae9f2a857748
Gustav87and <gustav87and@gmail.com>
2016-10-24 11:31:37 +0200

    Adding list of countries to myDestinations and adding functionality of choosing 3 favorite countries

--- src/main/resources/static/myDestinations/scripts/myDestinations.controller.js
+++ src/main/resources/static/myDestinations/scripts/myDestinations.controller.js
@@ -29,5 +29,53 @@ myDestinations.destinations.myDestinationsController = function(myDestinationsSe
     }
 
     this.object;
+
+
+    this.chosenCountries = [];
+
+    var add_button = document.getElementById("addButton");
+    var submit_button = document.getElementById("submitButton");
+
+    this.addItemToList = function () {
+        self.chosenCountries.push(self.aCountry);
+        console.log(self.aCountry);
+        var indexOfAddedItem = self.object.listDestinations.indexOf(self.aCountry);
+        self.object.listDestinations.splice(indexOfAddedItem, 1);
+        console.log(self.chosenCountries);
+        self.aCountry = this.object.listDestinations[0]; // Resets dropdown list to position 0 after each add
+        if (self.chosenCountries.length >= 3) { // Disables add and enables submit after 3 added countries
+            add_button.setAttribute("disabled", true);
+            submit_button.removeAttribute("disabled");
+        }
+    };
+
+    this.removeItemFromList = function (listitem) {
+        var indexOfItem = self.chosenCountries.indexOf(listitem);
+        self.object.listDestinations.push(listitem);
+        console.log(listitem);
+        if (self.chosenCountries.length >= 3) { // Enables add and disables submit when countries in array no longer are 3
+            add_button.removeAttribute("disabled");
+            submit_button.setAttribute("disabled", true);
+        }
+        if (indexOfItem >= 0) {
+            self.chosenCountries.splice(indexOfItem, 1);
+        }
+    };
+
+    this.sendForm = function () {
+        var sendObject = {
+            user: {firstname: "gustav", lastname: "andersson", username: "gustav87", userID:15},
+            countries: [this.chosenCountries[0].id, this.chosenCountries[1].id, this.chosenCountries[2].id]
+        };
+        self.loading = true;
+        console.log(self.chosenCountries);
+        console.log("----");
+        console.log(sendObject);
+        $http.post("/myDestinations",sendObject)
+        .then(function(response) {
+            $rootScope.user = response.data.user;
+            console.log($rootScope.user.personalityType)
+        });
+    };
 }
 
