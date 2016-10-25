07af28333d20c91e9ccf0333375a063fe6a345aa
Josefin Karlgrund <josefin.karlgrund@gmail.com>
2016-10-25 14:15:11 +0200

    Destination Suggestions - show one at a time

--- src/main/resources/static/suggestions/scripts/destinationSuggestions.controller.js
+++ src/main/resources/static/suggestions/scripts/destinationSuggestions.controller.js
@@ -8,18 +8,25 @@ suggestions.destinationSuggestions.destinationSuggestionsController = function(d
     var self = this;
 
     this.getObject = function () {
-
-        console.log($rootScope.user);
+        self.loading=true;
         $http.post("/mySuggestions",$rootScope.user).then(function (response) {
                 self.object = response.data;
+            self.currentSuggestion=response.data.listDestinations[self.index];
             });
+        self.loading=false;
     }
 
-    this.like=function() {
+    this.nextSuggestion=function() {
+        if (self.index<(self.object.listDestinations.length-1)){
+            self.index++;
+        } else{self.index=0;}
+        self.currentSuggestion=self.object.listDestinations[self.index];
+    };
 
-    }
     this.object;
-
+    this.currentSuggestion;
+    this.loading=false;
+    this.index=0;
 }
 
 
