c7ab9e3de635c24ca332e7b22fe35a93e1dc9985
Martin Portin <martinportin@gmail.com>
2016-10-27 18:37:02 +0200

    Commit with packages I.

---
+++ src/main/java/com/sallskapsresanExceptionHandling/RuntimeError.java
@@ -0,0 +1,10 @@
+package com.sallskapsresanExceptionHandling;
+
+import com.sallskapsresanExceptionHandling.GeneralError;
+
+/**
+ * Created by Administrator on 2016-10-26.
+ */
+public class RuntimeError extends GeneralError {
+
+}
---
+++ src/main/java/com/sallskapsresanExceptionHandling/UserValidationError.java
@@ -0,0 +1,25 @@
+package com.sallskapsresanExceptionHandling;
+
+/**
+ * Created by Administrator on 2016-10-24.
+ */
+public class UserValidationError extends GeneralError {
+    private String field;
+    private String code;
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
